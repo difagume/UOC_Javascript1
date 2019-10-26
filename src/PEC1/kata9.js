fb97c933f5df37e21b0c18a0d31df63fa14e978f
Diego Fabricio <difagume@gmail.com>
2019-10-25 22:45:34 -0500

    kata 9

--- src/PEC1/kata9.js
+++ src/PEC1/kata9.js
@@ -1,11 +1,9 @@
-function Arbol(especie, fruta) {
-  this.especie = especie;
-  this.fruta = fruta;
-}
-
 export default function plantarArbol(especie, fruta) {
   if (typeof especie === 'string' && typeof fruta === 'string') {
-    Object.create(Arbol);
+    function Arbol(especie, fruta) {
+      this.especie = especie;
+      this.fruta = fruta;
+    }
     return new Arbol(especie, fruta);
   } else return null;
 }
