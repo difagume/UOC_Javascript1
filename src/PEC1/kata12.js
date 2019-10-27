58055fd554a31524a2c4d2e74f8d9195becc1b6a
Diego Fabricio <difagume@gmail.com>
2019-10-26 20:37:28 -0500

    kata12

---
+++ src/PEC1/kata12.js
@@ -0,0 +1,49 @@
+export default function plantarArbol(especie, fruta) {
+  let f;
+  let e;
+
+  String.prototype.presentarArbol = function(fruta) {
+    if (fruta === undefined) {
+      console.log(`Este árbol es un ${e}`);
+      return `Este árbol es un ${e}`;
+    } else {
+      console.log(`Este árbol es un ${e} y da ${fruta}`);
+      return `Este árbol es un ${e} y da ${fruta}`;
+    }
+  };
+
+  if (typeof especie === 'string' && typeof fruta === 'string') {
+    e = especie;
+    f = fruta;
+
+    var arbol = {
+      get obtenerFruta() {
+        return f;
+      },
+
+      get obtenerEspecie() {
+        return e;
+      },
+
+      set definirFruta(val) {
+        if (typeof val === 'string') {
+          const er = new RegExp(val.substr(0, 4));
+
+          if (e.search(er) === 0) {
+            f = val;
+          } else {
+            throw new Error();
+          }
+        } else {
+          throw new Error();
+        }
+      },
+
+      set definirEspecie(val) {
+        if (typeof val === 'string') e = val;
+      }
+    };
+
+    return arbol;
+  } else return null;
+}
---
+++ src/PEC1/kata12.spec.js
@@ -0,0 +1,19 @@
+import plantarArbol from './kata12';
+
+describe('Kata #12: expresiones regulares', () => {
+  test(`Al crear un arbol con los valores manzano y manzana e invocar posteriormente
+  definirFruta con el valor pera el método lanza un error y mantiene su valor previo`, () => {
+    const arbol = plantarArbol('manzano', 'manzana');
+    expect(() => {
+      arbol.definirFruta = 'pera';
+    }).toThrow();
+    expect(arbol.obtenerFruta).toBe('manzana');
+  });
+
+  test(`Al crear un arbol con los valores peral y manzana e invocar posteriormente
+  definirFruta con el valor pera el método modifica el valor de la propiedad fruta`, () => {
+    const arbol = plantarArbol('peral', 'manzana');
+    arbol.definirFruta = 'pera';
+    expect(arbol.obtenerFruta).toBe('pera');
+  });
+});
